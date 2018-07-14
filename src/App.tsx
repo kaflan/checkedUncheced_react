fbe0db46a8e5b83f5206243df0c78611d3979f02
kaflan <kaf_90@mail.ru>
2018-07-15 00:37:50 +0300

    clening code add test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import CheckboxGroup from './componentrs/GroupCheckbox/GroupCheckbox';
+import CheckboxGroup from './componentrs/CheckboxGroup';
 
 class App extends React.Component {
   public render() {
---
+++ src/componentrs/__test__/GroupCheckox.test.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { configure, mount } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+import CheckboxGroup from "../CheckboxGroup";
+
+describe(`${<CheckboxGroup />}`, ()=> {
+  it('render form', () => {
+    const wrapper = mount(<CheckboxGroup />);
+    expect(wrapper.find("form")).toHaveLength(1)
+  })
+  
+})
