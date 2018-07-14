94ead061690b831f49d919b24066366453f6d821
kaflan <kaf_90@mail.ru>
2018-07-14 23:55:41 +0300

    small refactoring and clean code

--- src/componentrs/GroupCheckbox/Checkbox.tsx
+++ src/componentrs/GroupCheckbox/Checkbox.tsx
@@ -7,36 +7,16 @@ interface Props {
     checked: boolean,
     id?: string
 }
-interface State {
-    value: string,
-    checked: boolean
-}
 
-class Checkbox extends React.Component <Props, State> {
-    public static getDerivedStateFromProps(props:any, state: any) {
-        if(props) {
-            return { checked: props.checked}
-        }
-        return null;
-    }
-    constructor (props: Props) {
-        super(props)
-        this.state = {
-            checked: props.checked,
-            value: props.value
-        }
-    }
+class Checkbox extends React.Component <Props> {
+
     
     public handleChange = (event: React.FormEvent<HTMLElement>) :void =>  {
         const { currentTarget: {id} } = event;
-        this.setState(({ checked }) => (
-            {
-                checked: !checked,
-            }
-        ), () => this.props.onChange(id));
+        this.props.onChange(id)
     }
     public render() {
-      const { state: {value, checked}, props: {label, id} } = this;
+      const { props: {label, id, value, checked} } = this;
       return (
         <label>
           <input type="checkbox" value={value} onChange={this.handleChange} checked={checked}  id={id} />
--- src/componentrs/GroupCheckbox/GroupCheckbox.tsx
+++ src/componentrs/GroupCheckbox/GroupCheckbox.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as faker from 'faker';
-import {Item} from './models';
+import { Item } from './models';
 import Chekbox from './Checkbox';
 import './index.css'
 
-function fakeData () :Item[] {
+function fakeData(): Item[] {
   const result: Item[] = [];
-  for(let i = 0; i<= 10; i++) {
+  for (let i = 0; i <= 10; i++) {
     const item: Item = {
       id: faker.random.uuid(),
       value: faker.name.firstName(),
@@ -16,72 +16,68 @@ function fakeData () :Item[] {
     result.push(item)
   }
   return result;
-} 
+}
 
 interface State {
   selectCheckbox: string[],
   checboxes: Item[]
 }
-class CheckboxGroup extends React.Component<any,State> {
-    constructor (props:any) {
-      super(props);
-      this.state = {
-        selectCheckbox: [],
-        checboxes: fakeData(), 
-      }
-    }
-    public handleCheckbox = (id: string) => {
-      const { checboxes } = this.state;
-      const selected = checboxes.map(item => {
-        if (item.id === id) {
-          return ({
-            ...item,
-            checked: !item.checked
-          })
-        }
-        return {...item};
-      }); 
-      this.setState({
-        checboxes: selected,
-        selectCheckbox: this.selectCheckbox(selected),
-      })
-    }
-    public selectCheckbox = (selected: any[]) => selected
-    .filter((item: Item) => item.checked === true)
-    .map((item)=>{
-      return item.id;
-    });
-
-    public onCheckedAll = (checked: boolean): any => {
-      const {checboxes} = this.state;
-      const selected:any = checboxes.map(item=>({
-        ...item,
-        checked
-      }));
-      
-      this.setState({
-        selectCheckbox: this.selectCheckbox(selected),
-        checboxes: selected
-      })
-    }
-    public selectAll = ()=> {
-      this.onCheckedAll(true)
-    }
-    public diselectAll = () => {
-      this.onCheckedAll(false)
-    }
-    public render() {
-      const {state: {checboxes},  selectAll, diselectAll} = this;
-      // console.log(checboxes);
-      
-      return (
-        <div className="flex">
-         {checboxes.map(item=> <Chekbox key={item.id} id={item.id} value={item.value} checked={item.checked} label={item.label} onChange={this.handleCheckbox}  />)}
-          <button onClick={selectAll}>Select all</button>
-          <button onClick={diselectAll} >Diselect all</button>
-        </div>
-      );
+class CheckboxGroup extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      selectCheckbox: [],
+      checboxes: fakeData(),
     }
   }
+
+  public handleCheckbox = (id: string) => {
+    const { checboxes } = this.state;
+    const selected = checboxes.map(item => {
+      if (item.id === id) {
+        return ({
+          ...item,
+          checked: !item.checked
+        })
+      }
+      return { ...item };
+    });
+    this.setState({
+      checboxes: selected,
+      selectCheckbox: this.selectCheckbox(selected),
+    })
+  }
   
-  export default CheckboxGroup;
+  public selectCheckbox = (selected: Item[]): string[] => selected.filter((item: Item) => item.checked === true).map((item: Item) => { return item.id; });
+
+  public onCheckedAll = (checked: boolean): void => {
+    const { checboxes } = this.state;
+    const selected: any = checboxes.map(item => ({
+      ...item,
+      checked
+    }));
+
+    this.setState({
+      selectCheckbox: this.selectCheckbox(selected),
+      checboxes: selected
+    })
+  }
+  public selectAll = () => {
+    this.onCheckedAll(true)
+  }
+  public diselectAll = () => {
+    this.onCheckedAll(false)
+  }
+  public render() {
+    const { state: { checboxes }, selectAll, diselectAll } = this;
+    return (
+      <form className="flex">
+        {checboxes.map(item => <Chekbox key={item.id} id={item.id} value={item.value} checked={item.checked} label={item.label} onChange={this.handleCheckbox} />)}
+        <button onClick={selectAll}>Select all</button>
+        <button onClick={diselectAll} >Diselect all</button>
+      </form>
+    );
+  }
+}
+
+export default CheckboxGroup;
